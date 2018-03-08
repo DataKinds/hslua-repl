935279a73cc50fb9839eaeb850a7d5d1ef4fcde1
T L <tslimkemann42@gmail.com>
2018-03-07 21:19:38 -0700

    finished __replShow()

--- src/LuaPrelude.hs
+++ src/LuaPrelude.hs
@@ -7,9 +7,28 @@ import Data.Text (unpack, Text)
 
 luaPrelude :: String
 luaPrelude = unpack [text|
-function __replPrint(object)
+function __replShow(object)
+    out = ""
     if (object ~= nil) then
-        print("=> " .. object)
+        if (type(object) == "string") then
+            out = "\"" .. object .. "\""
+        elseif (type(object) == "number") then
+            out = tostring(object)
+        elseif (type(object) == "function") then
+            out = "function"
+        elseif (type(object) == "userdata") then
+            out = "userdata"
+        elseif (type(object) == "table") then
+            out = out .. "{\n"
+            for k,v in pairs(object) do
+                out = out .. "  " .. __replShow(k) .. " = " ..  __replShow(v) .. "\n"
+            end
+            out = out .. "}"
+        end 
     end
+    return out
+end
+function __replPrint(object)
+    print("=> " .. __replShow(object))
 end
 |]

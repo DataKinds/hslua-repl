c588016b84513b5844c5f01205a7a5ac1e9845dd
T L <tslimkemann42@gmail.com>
2018-03-08 23:14:03 -0700

    fixed weird scoping, added __replGlobalNames()

--- src/LuaPrelude.hs
+++ src/LuaPrelude.hs
@@ -11,24 +11,22 @@ function __replShow(object, depth)
     maxDepth = 10
     out = ""
     if (depth < maxDepth) then
-        if (object ~= nil) then
-            if (type(object) == "string") then
-                out = "\"" .. object .. "\""
-            elseif (type(object) == "number") then
-                out = tostring(object)
-            elseif (type(object) == "function") then
-                out = "<function>"
-            elseif (type(object) == "userdata") then
-                out = "<userdata>"
-            elseif (type(object) == "table") then
-                out = out .. "{\n"
-                for k,v in pairs(object) do
-                    out = out .. string.rep("  ", depth + 1) .. __replShow(k, depth + 1) .. " = " ..  __replShow(v, depth + 1) .. "\n"
-                end
-                out = out .. string.rep("  ", depth) .. "}"
-            end
-        else
+        if (type(object) == "nil") then
             out = "nil"
+        elseif (type(object) == "string") then
+            out = "\"" .. object .. "\""
+        elseif (type(object) == "number") then
+            out = tostring(object)
+        elseif (type(object) == "function") then
+            out = "<function>"
+        elseif (type(object) == "userdata") then
+            out = "<userdata>"
+        elseif (type(object) == "table") then
+            out = out .. "{\n"
+            for k,v in pairs(object) do
+                out = out .. string.rep("  ", depth + 1) .. __replShow(k, depth + 1) .. " = " ..  __replShow(v, depth + 1) .. "\n"
+            end
+            out = out .. string.rep("  ", depth) .. "}"
         end
     else
         out = "... <max print depth> ..."
@@ -37,5 +35,7 @@ function __replShow(object, depth)
 end
 function __replPrint(object)
     print("=> " .. __replShow(object, 0))
+end
+function __replGlobalNames()
 end
 |]
